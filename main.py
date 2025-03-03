f996407f3459204c97e7263de15e5e1ba78a8448
Maria Fernanda Moroney Sole <maria.moroney@ulead.ac.cr>
2025-03-03 10:22:11 -0600

    Agregada nueva ruta para usuarios

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/usuarios/{usuario_id}")
+def read_usuario(usuario_id: int):
+    return {"usuario_id": usuario_id, "nombre": "Usuario de ejemplo"}
